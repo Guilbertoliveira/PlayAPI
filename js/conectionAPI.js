4a99b097efcc9e0e11451ff76998df218520a598
Guilbert Oliveira <41201436+Guilbertoliveira@users.noreply.github.com>
2023-03-20 14:34:30 -0300

    create get and post function

--- js/conectionAPI.js
+++ js/conectionAPI.js
@@ -11,8 +11,27 @@ async function listaVideos(){
     return conexaoConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch('http://localhost:3000/videos', {
+        method: "POST",
+        headers: { //content-type especificar que tipo de arquivo ta sendo enviado ou recebendo
+            "content-type": "aplicacation/json"
+        }, 
+        body: JSON.stringify({ // corpo da requisicao, enviando um objeto porém como é requisição precisa ser string
+            titulo: titulo, //dados que queremos cadastrar na requisicao
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
 
 
